857f6279fc270b399def2605f9d09b895193c803
decemyn <radulescudecebal@outlook.com>
2021-04-30 14:21:24 +0300

    Create initial project structure

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "headers/basewindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    BaseWindow w;
+    w.show();
+    return a.exec();
+}
---
+++ source/basewindow.cpp
@@ -0,0 +1,15 @@
+#include "headers/basewindow.h"
+#include "ui_basewindow.h"
+
+BaseWindow::BaseWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::BaseWindow)
+{
+    ui->setupUi(this);
+}
+
+BaseWindow::~BaseWindow()
+{
+    delete ui;
+}
+
