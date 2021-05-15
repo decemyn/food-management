4a386a7645ae3499df3d2115962bc40efd205b43
decemyn <radulescudecebal@outlook.com>
2021-05-15 19:23:56 +0300

    Removed unecessary includes

--- source/basewindow.cpp
+++ source/basewindow.cpp
@@ -1,12 +1,5 @@
 #include "headers/basewindow.h"
 #include "ui_basewindow.h"
-#include <QDebug>
-#include <QMessageBox>
-#include <QString>
-#include <headers/fileio.h>
-#include <headers/product.h>
-#include <string>
-#include <vector>
 BaseWindow::BaseWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::BaseWindow) {
   ui->setupUi(this);
